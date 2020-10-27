90c01f511b5f2a315679f08281cdfd86bd172cd5
Daniel Garnier-Moiroux <git@garnier.wf>
2020-10-27 15:12:05 +0100

    add template file + test for parse_graph

---
+++ dijkstra.py
@@ -0,0 +1,34 @@
+def parse_graph(filename):
+    """
+    constructs a graph representation from a file
+
+    Parameters:
+      filename: the name of the file to read from
+    Returns:
+      a dict representing the graph
+    """
+    return {} # TODO: votre code ici !
+
+def number_vertices(graph):
+    """
+    returns number of vertices
+    
+    Parameters:
+      graph: implemented as a dictionary of adjacency dictionaries
+      
+    Returns:
+      int: number of vertices
+    """
+    return -1 # TODO: votre code ici !
+
+def reachables(graph, s):
+    """
+    computes the set of reachable vertices in a graph from source s
+
+    Parameters:
+      graph: a graph implemented as a dict of adjacency dicts
+      s: the source vertex
+    Returns:
+      a set of vertices in graph
+    """
+    return set() # TODO: votre code ici !
---
+++ test_parse_graph.py
@@ -0,0 +1,17 @@
+import unittest
+from ../dijkstra import parse_graph
+
+expected_graph = {'a': {'b': 7, 'd': 9, 'c': 14},
+     'b': {'d': 10, 'e': 15},
+     'c': {'d': 2, 'f': 9},
+     'd': {'e': 11},
+     'e': {'f': 6}}
+
+class TestParseGraph(unittest.TestCase):
+
+    def test_parse_graph(self):
+        parsed = parse_graph("graph.csv")
+        self.assertEqual(parsed, expected_graph)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
